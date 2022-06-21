8edd49ba9561b3b00aa6db2525ccca0e89fb9b23
Rohan Tabish <rohan.tabish@intel.com>
2022-06-20 17:56:15 -0700

    Add the collected data

--- sweep_cas_benchmarks/solo_sweep_mlc_emon.sh
+++ sweep_cas_benchmarks/solo_sweep_mlc_emon.sh
@@ -10,59 +10,65 @@ pqos -R
 #cores=12
 cpupower frequency-set -u 2300000 -d 2300000
 
-for cores in {11..55..2}
+pattern=2
+
+for cores in {13..55..14}
 do
-for pattern in {2..12..1}
-do
-   for casValue in {15..255..30}
+#for pattern in {6..12..6}
+#do
+   for casValue in {255..255..30}
    do
       cd $PWD/hwdrc_postsi/scripts
-      ./hwdrc_icx_2S_xcc_init_to_default_pqos_CAS.sh $casValue 0-$cores 56-57
+      #./hwdrc_icx_2S_xcc_init_to_default_pqos_CAS.sh $casValue 0-$cores 56-57
+      ./hwdrc_icx_2S_xcc_disable.sh
       cd -
       echo "Starting EMON"
-      emon -i spr-events.txt >> ${cores}_W_${pattern}_emon.dat &
+      emon -i spr-events.txt >> ${cores}_W_${pattern}_${casValue}_emon.dat &
       #mlc --loaded_latency -W6 -t60 -d0 -k1-$cores | tail -1 | awk '{print $2,$3}' > temp_mlc
       mlc --loaded_latency -W$pattern -t60 -c0 -k1-$cores -d0  | tail -1 | awk '{print $2,$3}' > temp_mlc
       #mlc --loaded_latency -W2 -t60 -c0 -k1-$cores -d0 | tail -1 | awk '{print $2,$3}' > temp_mlc
       emon -stop
       
-      mlcBwScore=$(cat temp_mlc | awk '{print $1}')
-      mlcLaScore=$(cat temp_mlc | awk '{print $2}')
+      mlcLaScore=$(cat temp_mlc | awk '{print $1}')
+      mlcBwScore=$(cat temp_mlc | awk '{print $2}')
       rm -rf temp_mlc
       
       #disable HWDRC to speed up the processing of emon
       cd $PWD/hwdrc_postsi/scripts
       ./hwdrc_icx_2S_xcc_disable.sh
       cd -
- 
       
       #Process EMON Data
- 
 
       cd edp
 
-      rm -rf *.dat
+      #rm -rf *.dat
       rm -rf *.csv
       rm -rf *.output
 
-      mv ../${cores}_W_${pattern}_emon.dat .
+      mv ../${cores}_W_${pattern}_${casValue}_emon.dat .
 
-      ruby edp.rb -i ${cores}_W_${pattern}_emon.dat -m spr-2s.xml -o ${cores}_W_${pattern}_emon_out.output --socket-view
+      ruby edp.rb -i ${cores}_W_${pattern}_${casValue}_emon.dat -m spr-2s.xml -o ${cores}_W_${pattern}_${casValue}_emon_out.output --socket-view
 
 
-      CAS_READ=$(grep -ir UNC_M_CAS_COUNT.RD __edp_socket_view_summary.csv | cut -d',' -f3) 
-      CAS_WRITE=$(grep -ir UNC_M_CAS_COUNT.WR __edp_socket_view_summary.csv | cut -d',' -f3) 
-      CAS_ALL=$(grep -ir UNC_M_CAS_COUNT.ALL __edp_socket_view_summary.csv | cut -d',' -f3) 
+      CAS_READ=$(xlsx2csv ${cores}_W_${pattern}_${casValue}_emon_out.output.xlsx | grep UNC_M_CAS_COUNT.RD |  cut -d',' -f2)
+      CAS_WRITE=$(xlsx2csv  ${cores}_W_${pattern}_${casValue}_emon_out.output.xlsx | grep UNC_M_CAS_COUNT.WR |  cut -d',' -f2)
+      CAS_ALL=$(xlsx2csv  ${cores}_W_${pattern}_${casValue}_emon_out.output.xlsx | grep UNC_M_CAS_COUNT.ALL |  cut -d',' -f2)
+     
+      READ_BW=$(xlsx2csv ${cores}_W_${pattern}_${casValue}_emon_out.output.xlsx | grep "metric_memory bandwidth read" |  cut -d',' -f2)
+      WRITE_BW=$(xlsx2csv  ${cores}_W_${pattern}_${casValue}_emon_out.output.xlsx | grep "metric_memory bandwidth write" |  cut -d',' -f2)
+      TOTAL_BW=$(xlsx2csv  ${cores}_W_${pattern}_${casValue}_emon_out.output.xlsx | grep "metric_memory bandwidth total" |  cut -d',' -f2)
+     
 
 
       cd -
 
-      echo $cores,$casValue,$mlcBwScore,$mlcLaScore,$CAS_READ,$CAS_WRITE,$CAS_ALL >> ${cores}_W_${pattern}_mlc_monitoring.csv
+      echo $cores,$casValue,$mlcBwScore,$mlcLaScore,$CAS_READ,$CAS_WRITE,$CAS_ALL,$READ_BW,$WRITE_BW,$TOTAL_BW >> cas_mlc_solo_read_write_log/${cores}_W_${pattern}__mlc_monitoring.csv
 
    #done
 #echo $cores
 done
-done 
+#done 
 done
 #echo $cores
 #done
