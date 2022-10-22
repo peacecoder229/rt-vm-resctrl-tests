8bde5ee353583c8bf4b3e4c47580df7465c6aa0a
root <root@JF5300-B11A346T.jf.intel.com>
2022-10-22 12:12:19 -0700

    Clean up RDMA

--- sweep_cas_benchmarks/rdma/test_rdma.sh
+++ sweep_cas_benchmarks/rdma/test_rdma.sh
@@ -9,4 +9,8 @@ time=$1
 
 ssh root@10.242.51.105 "qperf -t $time -cm1 192.168.232.254 rc_bw rc_lat" >> rdma_statistics
 
+BW=$(cat rdma_statistics | grep GB |  awk '{print $3}')
+lat=$(cat rdma_statistics | grep us |  awk '{print $3}')
+
+echo $BW,$lat
 
