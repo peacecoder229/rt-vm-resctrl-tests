cc74b1ae9593a5de57784600cff3f31ae956e701
root <root@JF5300-B11A346T.jf.intel.com>
2022-10-22 05:02:57 -0700

    Update the epoll test case

--- f-stack/example/test_main_epoll_scaling.sh
+++ f-stack/example/test_main_epoll_scaling.sh
@@ -1,14 +1,24 @@
 #!/bin/bash
 
-#Note: Please adjust the lcoremask parameter in config.ini file if you change number of processes
-./helloworld_epoll --conf config.ini --proc-type=primary --proc-id=0  9000 &
+killall -9 helloworld_epoll
 
 sleep 5
-I
-for i in {1..1}
+ifconfig ens11 down
+../dpdk/usertools/dpdk-devbind.py -u 38:00.0
+../dpdk/usertools/dpdk-devbind.py -b mlx5_core 38:00.0
+
+sleep 5
+
+#Note: Please adjust the lcoremask parameter in config.ini file if you change number of processes
+./helloworld_epoll --conf config.ini --proc-type=primary --proc-id=0  10000 &
+
+sleep 10
+
+for i in {1..2}
 do
-  port=$((9000+i))
+  port=$((10000+i))
   ./helloworld_epoll --conf config.ini --proc-type=secondary --proc-id=$i  $port &
   echo $port
+  sleep 5
 done
 
